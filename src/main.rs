9b273eda76703ab3a26b6cad614ec0159d853f06
David Ripton <dripton@ripton.net>
2022-05-08 11:30:17 -0400

    Add download_sector_data and a test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use std::path::PathBuf;
-
 use anyhow::Result;
 use clap::Parser;
 use clap_verbosity_flag;
+use std::ffi::OsString;
+use std::fs::{create_dir_all, read_dir, write};
+use std::io;
+use std::path::PathBuf;
+extern crate reqwest;
+use tempfile::tempdir;
+use url::Url;
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -11,8 +16,9 @@ struct Args {
     sector: Vec<String>,
 
     /// Path to input matrix in numpy ndy format
-    #[clap(short, long)]
-    data_directory: Option<PathBuf>,
+    // TODO Make this optional and use a tempdir if not provided
+    #[clap(short, long, required = true)]
+    data_directory: PathBuf,
 
     /// Path to output distance and predecessor matrixes in numpy ndz format
     #[clap(short, long, default_value = "/var/tmp")]
@@ -22,9 +28,85 @@ struct Args {
     verbose: clap_verbosity_flag::Verbosity,
 }
 
+fn download_sector_data(data_dir: &PathBuf, sector_names: &Vec<String>) -> Result<()> {
+    for sector_name in sector_names {
+        let sector_data_filename = sector_name.to_owned() + ".sec";
+        let mut data_path = data_dir.clone();
+        data_path.push(sector_data_filename);
+        let sector_xml_filename = sector_name.to_owned() + ".xml";
+        let mut metadata_path = data_dir.clone();
+        metadata_path.push(sector_xml_filename);
+        let base_url = Url::parse("https://travellermap.com/data/")?;
+        if !data_path.exists() {
+            let data_url = base_url.join(sector_name)?;
+            let body = reqwest::blocking::get(data_url)?.text()?;
+            write(data_path, body)?;
+        }
+        if !metadata_path.exists() {
+            let metadata_url = base_url.join(&(sector_name.to_owned() + "/metadata"))?;
+            let body = reqwest::blocking::get(metadata_url)?.text()?;
+            write(metadata_path, body)?;
+        }
+    }
+    Ok(())
+}
+
 fn main() -> Result<()> {
     let args = Args::parse();
-    println!("args {:?}", args);
+    let output_dir = args.output_directory;
+    let data_dir = args.data_directory;
+    let sector_names = args.sector;
 
+    create_dir_all(&output_dir)?;
+    create_dir_all(&data_dir)?;
+
+    download_sector_data(&data_dir, &sector_names)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_download_sector_data() -> Result<()> {
+        let mut expected_filenames = Vec::new();
+        let sector_names = vec![
+            "Deneb".to_string(),
+            "Gvurrdon".to_string(),
+            "Spinward Marches".to_string(),
+        ];
+        for sector_name in &sector_names {
+            expected_filenames.push(sector_name.to_owned() + ".sec");
+            expected_filenames.push(sector_name.to_owned() + ".xml");
+        }
+        expected_filenames.sort();
+
+        let temp_dir = tempdir()?;
+        download_sector_data(&(temp_dir.path().to_path_buf()), &sector_names)?;
+        let found_filename_results: Vec<Result<OsString, io::Error>> = read_dir(&temp_dir)?
+            .map(|res| res.map(|e| e.file_name()))
+            .collect();
+        let mut found_os_filenames: Vec<OsString> = Vec::new();
+        for res in found_filename_results {
+            if let Ok(filename) = res {
+                found_os_filenames.push(filename);
+            }
+        }
+
+        let mut found_filenames: Vec<String> = Vec::new();
+        for osstr in found_os_filenames {
+            let opt = osstr.to_str();
+            if let Some(st) = opt {
+                found_filenames.push(st.to_string());
+            }
+        }
+        found_filenames.sort();
+
+        assert_eq!(expected_filenames, found_filenames);
+
+        temp_dir.close()?;
+
+        Ok(())
+    }
 }
