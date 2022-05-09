86ce105b34200d93c3ca407f1b627d747f568c8f
David Ripton <dripton@ripton.net>
2022-05-08 21:57:49 -0400

    Work in progress toward building Sectors

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use anyhow::Result;
+use anyhow::{Error, Result};
 use clap::Parser;
 use clap_verbosity_flag;
-use std::ffi::OsString;
-use std::fs::{create_dir_all, read_dir, write};
-use std::io;
+use elementtree::Element;
+use std::collections::HashMap;
+use std::fs::{create_dir_all, write, File};
 use std::path::PathBuf;
 extern crate reqwest;
-use tempfile::tempdir;
 use url::Url;
 
 #[derive(Debug, Parser)]
@@ -51,6 +50,125 @@ fn download_sector_data(data_dir: &PathBuf, sector_names: &Vec<String>) -> Resul
     Ok(())
 }
 
+#[derive(Debug)]
+struct World {
+    // TODO
+}
+
+#[derive(Debug)]
+struct Sector {
+    names: Vec<String>,
+    abbreviation: String,
+    location: (i64, i64),
+    subsector_letter_to_name: HashMap<String, String>,
+    allegience_code_to_name: HashMap<String, String>,
+    hex_to_world: HashMap<String, World>,
+}
+
+impl Sector {
+    fn new(
+        data_dir: &PathBuf,
+        sector_name: String,
+        location_to_sector: &mut HashMap<(i64, i64), Sector>,
+    ) -> Sector {
+        let names = Vec::new();
+        let abbreviation = "".to_string();
+        let location = (-1, -1);
+        let subsector_letter_to_name = HashMap::new();
+        let allegience_code_to_name = HashMap::new();
+        let hex_to_world = HashMap::new();
+        let mut sector = Sector {
+            names,
+            abbreviation,
+            location,
+            subsector_letter_to_name,
+            allegience_code_to_name,
+            hex_to_world,
+        };
+
+        sector.parse_xml_metadata(&data_dir, &sector_name);
+        sector.parse_column_data(&data_dir, &sector_name);
+        sector
+    }
+
+    fn parse_xml_metadata(&mut self, data_dir: &PathBuf, sector_name: &str) -> Result<()> {
+        let mut xml_path = data_dir.clone();
+        xml_path.push(sector_name.to_owned() + ".xml");
+        let xml_file = File::open(xml_path)?;
+        let root = Element::from_reader(xml_file)?;
+
+        let abbreviation_opt = root.get_attr("Abbreviation");
+        if let Some(abbreviation) = abbreviation_opt {
+            self.abbreviation = abbreviation.to_string();
+        }
+
+        let mut x = i64::MAX;
+        let x_opt = root.find("X");
+        if let Some(x_element) = x_opt {
+            let x_text = x_element.text();
+            x = x_text.parse()?;
+        }
+        let mut y = i64::MAX;
+        let y_opt = root.find("y");
+        if let Some(y_element) = y_opt {
+            let y_text = y_element.text();
+            y = y_text.parse()?;
+        }
+        self.location = (x, y);
+
+        let name_elements = root.find_all("Name");
+        for name_element in name_elements {
+            if name_element.text().len() > 0 {
+                self.names.push(name_element.text().to_string());
+            }
+        }
+
+        let subsectors_opt = root.find("Subsectors");
+        if let Some(subsectors_element) = subsectors_opt {
+            let subsector_elements = subsectors_element.find_all("Subsector");
+            for subsector_element in subsector_elements {
+                let letter_opt = subsector_element.get_attr("Index");
+                if let Some(letter) = letter_opt {
+                    let subsector_name = subsector_element.text().to_string();
+                    if subsector_name.len() > 0 {
+                        self.subsector_letter_to_name
+                            .insert(letter.to_string(), subsector_name);
+                    }
+                }
+            }
+        }
+
+        let allegiances_opt = root.find("Allegiances");
+        if let Some(allegiances_element) = allegiances_opt {
+            let allegience_elements = allegiances_element.find_all("Allegience");
+            for allegience_element in allegience_elements {
+                let code_opt = allegience_element.get_attr("Code");
+                if let Some(code) = code_opt {
+                    let allegience_name = allegience_element.text().to_string();
+                    if allegience_name.len() > 0 {
+                        self.allegience_code_to_name
+                            .insert(code.to_string(), allegience_name);
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn parse_column_data(&self, data_dir: &PathBuf, sector_name: &str) {
+        // TODO
+    }
+
+    fn populate_neighbors(&self) {
+        // TODO
+    }
+
+    fn name(&self) -> &str {
+        &self.names[0]
+    }
+}
+
 fn main() -> Result<()> {
     let args = Args::parse();
     let output_dir = args.output_directory;
@@ -61,12 +179,19 @@ fn main() -> Result<()> {
     create_dir_all(&data_dir)?;
 
     download_sector_data(&data_dir, &sector_names)?;
+    let mut location_to_sector: HashMap<(i64, i64), Sector> = HashMap::new();
+
+    // TODO
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::ffi::OsString;
+    use std::fs::read_dir;
+    use std::io;
+    use tempfile::tempdir;
 
     #[test]
     fn test_download_sector_data() -> Result<()> {
