1a9387175853f992f466698a2522477ed22cf930
David Ripton <dripton@ripton.net>
2022-05-16 12:38:05 -0400

    Add test_distance_modifier

--- src/tests.rs
+++ src/tests.rs
@@ -709,6 +709,114 @@ mod tests {
     }
 
     #[rstest]
+    fn test_distance_modifier(data_dir: &PathBuf, download: &Result<Vec<String>>) -> Result<()> {
+        if let Ok(_sector_names) = download {};
+        let mut coords_to_world: HashMap<Coords, World> = HashMap::new();
+        let mut location_to_sector: HashMap<(i64, i64), Sector> = HashMap::new();
+        let spin = Sector::new(
+            &data_dir,
+            "Spinward Marches".to_string(),
+            &mut coords_to_world,
+        );
+        let dene = Sector::new(&data_dir, "Deneb".to_string(), &mut coords_to_world);
+        location_to_sector.insert(spin.location, spin.clone());
+        location_to_sector.insert(dene.location, dene.clone());
+        for sector in location_to_sector.values() {
+            sector
+                .parse_xml_routes(&data_dir, &location_to_sector, &mut coords_to_world)
+                .unwrap();
+        }
+        // Make a temporary clone to avoid having mutable and immutable refs.
+        let coords_to_world2 = coords_to_world.clone();
+        for world in coords_to_world.values_mut() {
+            world.populate_neighbors(&coords_to_world2);
+        }
+        let mut sorted_coords: Vec<Coords>;
+        sorted_coords = coords_to_world.keys().cloned().collect();
+        sorted_coords.sort();
+        assert_eq!(sorted_coords.len(), 825);
+        let mut coords_to_index: HashMap<Coords, usize> = HashMap::new();
+        for (ii, coords) in sorted_coords.iter_mut().enumerate() {
+            coords_to_index.insert(*coords, ii);
+            let world_opt = coords_to_world.get_mut(coords);
+            if let Some(world) = world_opt {
+                world.index = Some(ii);
+            } else {
+                panic!("World not found at coords");
+            }
+        }
+        let (dist2, _) = populate_navigable_distances(&sorted_coords, &coords_to_world, 2);
+
+        let aramis = spin
+            .hex_to_world("3110".to_string(), &coords_to_world)
+            .unwrap();
+        let ldd = spin
+            .hex_to_world("3010".to_string(), &coords_to_world)
+            .unwrap();
+        let natoko = spin
+            .hex_to_world("3209".to_string(), &coords_to_world)
+            .unwrap();
+        let vinorian = spin
+            .hex_to_world("3111".to_string(), &coords_to_world)
+            .unwrap();
+        let margesi = spin
+            .hex_to_world("3212".to_string(), &coords_to_world)
+            .unwrap();
+        let corfu = spin
+            .hex_to_world("2602".to_string(), &coords_to_world)
+            .unwrap();
+        let andor = spin
+            .hex_to_world("0236".to_string(), &coords_to_world)
+            .unwrap();
+        let candory = spin
+            .hex_to_world("0336".to_string(), &coords_to_world)
+            .unwrap();
+        let reno = spin
+            .hex_to_world("0102".to_string(), &coords_to_world)
+            .unwrap();
+        let regina = spin
+            .hex_to_world("1910".to_string(), &coords_to_world)
+            .unwrap();
+        let mongo = spin
+            .hex_to_world("1204".to_string(), &coords_to_world)
+            .unwrap();
+        let collace = spin
+            .hex_to_world("1237".to_string(), &coords_to_world)
+            .unwrap();
+        let pavanne = spin
+            .hex_to_world("2905".to_string(), &coords_to_world)
+            .unwrap();
+        let raweh = spin
+            .hex_to_world("0139".to_string(), &coords_to_world)
+            .unwrap();
+        let javan = dene
+            .hex_to_world("2131".to_string(), &coords_to_world)
+            .unwrap();
+        let salaam = dene
+            .hex_to_world("3213".to_string(), &coords_to_world)
+            .unwrap();
+
+        assert_eq!(aramis.distance_modifier(aramis, &dist2), 0.0);
+        assert_eq!(aramis.distance_modifier(ldd, &dist2), 0.0);
+        assert_eq!(aramis.distance_modifier(vinorian, &dist2), 0.0);
+        assert_eq!(aramis.distance_modifier(corfu, &dist2), 2.0);
+        assert_eq!(aramis.distance_modifier(andor, &dist2), 6.0);
+        assert_eq!(aramis.distance_modifier(margesi, &dist2), 1.0);
+        assert_eq!(aramis.distance_modifier(pavanne, &dist2), 1.5);
+        assert_eq!(aramis.distance_modifier(regina, &dist2), 2.0);
+        assert_eq!(aramis.distance_modifier(mongo, &dist2), 2.5);
+        assert_eq!(aramis.distance_modifier(collace, &dist2), 3.0);
+        assert_eq!(reno.distance_modifier(javan, &dist2), 3.5);
+        assert_eq!(andor.distance_modifier(candory, &dist2), 6.0);
+        assert_eq!(candory.distance_modifier(andor, &dist2), 6.0);
+        assert_eq!(ldd.distance_modifier(natoko, &dist2), 0.5);
+        assert_eq!(collace.distance_modifier(salaam, &dist2), 3.0);
+        assert_eq!(raweh.distance_modifier(salaam, &dist2), 3.5);
+
+        Ok(())
+    }
+
+    #[rstest]
     fn test_xboat_routes(data_dir: &PathBuf, download: &Result<Vec<String>>) -> Result<()> {
         if let Ok(_sector_names) = download {};
         let mut coords_to_world: HashMap<Coords, World> = HashMap::new();
